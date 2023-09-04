7689028a053f7358b59062e231130d129a41ec76
sileyekounou <venancekounou@gmail.com>
2023-09-04 14:54:31 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+        'pt': ['Pt Serif', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
